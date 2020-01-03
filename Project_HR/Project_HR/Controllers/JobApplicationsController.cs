462709e6f815287e3af74fc3a92c9ee6c54a84fc
290510 (Bartłomiej Truszkowski) <01130643@pw.edu.pl>
2020-01-03 17:20:01 +0100

    Added jobapplicationscotroller and linked to joboffercontroller

---
+++ Project_HR/Project_HR/Controllers/JobApplicationsController.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Project_HR.Models;
+
+namespace Project_HR.Controllers
+{
+    public class JobApplicationsController : Controller
+    {
+        private readonly DataContext _context;
+
+        public JobApplicationsController(DataContext context)
+        {
+            _context = context;
+        }
+
+        // GET: JobApplications
+        public async Task<IActionResult> Index()
+        {
+            var dataContext = _context.JobApplication.Include(j => j.Offer).Include(j => j.User);
+            return View(await dataContext.ToListAsync());
+        }
+
+        // GET: JobApplications/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var jobApplication = await _context.JobApplication
+                .Include(j => j.Offer)
+                .Include(j => j.User)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (jobApplication == null)
+            {
+                return NotFound();
+            }
+
+            return View(jobApplication);
+        }
+
+        // GET: JobApplications/Create
+        public IActionResult Create()
+        {
+            ViewData["OfferId"] = new SelectList(_context.JobOffer, "Id", "Description");
+            ViewData["UserId"] = new SelectList(_context.User, "Id", "EmailAdress");
+            return View();
+        }
+        //public async Task<IActionResult> Create(int? offerId)
+        //{
+        //    if (offerId == null)
+        //    {
+        //        return BadRequest($"id should not be null");
+        //    }
+        //    var model = new JobApplication();
+        //    model.Offer = await _context.JobOffer.FirstOrDefaultAsync(x => x.Id == offerId);
+        //    return View();
+        //}
+
+
+        // POST: JobApplications/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,OfferId,ContactAgreement,CvUrl,UserId")] JobApplication jobApplication)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(jobApplication);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["OfferId"] = new SelectList(_context.JobOffer, "Id", "Description", jobApplication.OfferId);
+            ViewData["UserId"] = new SelectList(_context.User, "Id", "EmailAdress", jobApplication.UserId);
+            return View(jobApplication);
+        }
+
+        // GET: JobApplications/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var jobApplication = await _context.JobApplication.FindAsync(id);
+            if (jobApplication == null)
+            {
+                return NotFound();
+            }
+            ViewData["OfferId"] = new SelectList(_context.JobOffer, "Id", "Description", jobApplication.OfferId);
+            ViewData["UserId"] = new SelectList(_context.User, "Id", "EmailAdress", jobApplication.UserId);
+            return View(jobApplication);
+        }
+
+        // POST: JobApplications/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,OfferId,ContactAgreement,CvUrl,UserId")] JobApplication jobApplication)
+        {
+            if (id != jobApplication.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(jobApplication);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!JobApplicationExists(jobApplication.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["OfferId"] = new SelectList(_context.JobOffer, "Id", "Description", jobApplication.OfferId);
+            ViewData["UserId"] = new SelectList(_context.User, "Id", "EmailAdress", jobApplication.UserId);
+            return View(jobApplication);
+        }
+
+        // GET: JobApplications/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var jobApplication = await _context.JobApplication
+                .Include(j => j.Offer)
+                .Include(j => j.User)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (jobApplication == null)
+            {
+                return NotFound();
+            }
+
+            return View(jobApplication);
+        }
+
+        // POST: JobApplications/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var jobApplication = await _context.JobApplication.FindAsync(id);
+            _context.JobApplication.Remove(jobApplication);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool JobApplicationExists(int id)
+        {
+            return _context.JobApplication.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ Project_HR/Project_HR/ViewModels/JobApplicationCreateView.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace Project_HR.Models
+{
+    public class JobApplicationCreateView : JobApplication
+    {
+        public IEnumerable<User> Users { get; set; }
+    }
+}
