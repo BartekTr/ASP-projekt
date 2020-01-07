496a00828bd9da4cdc9186b241561ed8acca0813
290510 (Bartłomiej Truszkowski) <01130643@pw.edu.pl>
2020-01-07 02:34:53 +0100

    Adding HR to JobOffer works

---
+++ Project_HR/Project_HR/Controllers/HRController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Project_HR.Models;
+
+namespace Project_HR.Controllers
+{
+    [Route("[controller]/[action]")]
+    public class HRController : Controller
+    {
+        private readonly DataContext _context;
+
+        public HRController(DataContext context)
+        {
+            _context = context;
+        }
+        // GET: HR
+        [ApiExplorerSettings(IgnoreApi = true)]
+        public async Task<IActionResult> Index(int id)
+        {
+            if (!User.Identity.IsAuthenticated || !User.IsInRole("Admin"))
+                return RedirectToAction("Index", "Home");
+            HRListViewModel model = new HRListViewModel
+            {
+                Users = _context.User.Where(x => x.Role.Name != "Admin").ToList(),
+                Offer = _context.JobOffer.FirstOrDefault(x => x.Id == id)
+            };
+
+            return View(model);
+        }
+        [HttpPost]
+        public async Task<ActionResult> Choose(int? offerId, int? userId)
+        {
+            if (offerId == null || userId == null)
+            {
+                return BadRequest($"id should not be null");
+            }
+            var user = await _context.User.FirstOrDefaultAsync(x => x.Id == userId);
+            user.RoleId = 2;
+            _context.Update(user);
+            var offer = await _context.JobOffer.FirstOrDefaultAsync(x => x.Id == offerId);
+            offer.Hrid = user.Id;
+            _context.Update(offer);
+            await _context.SaveChangesAsync();
+            return RedirectToAction("Details", "JobOffer", new { id=offerId});
+        }
+    }
+}
+
---
+++ Project_HR/Project_HR/ViewModels/HRListViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace Project_HR.Models
+{
+    public class HRListViewModel
+    {
+        public IEnumerable<User> Users{ get; set; }
+        public JobOffer Offer { get; set; }
+    }
+}
